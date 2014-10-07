0a2eac602f18d15c475ffa893c880be2905a1144
A.J. Fraser <ajamiesonfraser@gmail.com>
2014-10-07 12:08:12 -0300

    made chat usable on server and client siside

--- index.js
+++ index.js
@@ -7,7 +7,17 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+    console.log('message: ' + msg);
+  });
+});
+
+io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+	});
 });
 
 http.listen(3000, function(){
