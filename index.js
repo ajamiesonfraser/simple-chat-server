8cbf721ec52c317f28df3ffe9db1832622a7c9a2
A.J. Fraser <ajamiesonfraser@gmail.com>
2014-10-24 10:04:44 -0300

    rewrote comments, and integrated redis

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /* port declaration */
 var port= 5000;
+
 var express = require('express');
 var app = express();
 
@@ -7,12 +8,22 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
-/* Listen to port */
+var redis = require('redis');
+    client = redis.createClient();
+
+    /* Listen to port */
 console.log("listening on port " + port);
 server.listen(port);
 
+
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
+
+client.set("app name", "simple chat", redis.print);
+
 /* looks for request and response from the html (added _dirname because of multiple users) */
-app.get('/', function (req, res){
+app.get('/', function(req, res){
   res.sendfile(__dirname + '/index.html');
 });
 
@@ -24,11 +35,17 @@ app.use("/style.css", express.static(__dirname + '/style.css'));
 var usernames = {};
  
 io.sockets.on('connection', function (socket) {
- 
+    client.get('app name', function(err, reply) {
+        console.log('app name is', reply);
+    });
+    client.get('last chat', function(err, reply){
+        console.log('last chat', reply);
+    });
     // when the client emits 'sendchat', this listens and executes
     socket.on('sendchat', function (data) {
         // we tell the client to execute 'updatechat' with 2 parameters
         io.sockets.emit('updatechat', socket.username, data);
+        client.set('last chat', data);
     });
  
     // when the client emits 'adduser', this listens and executes
@@ -38,7 +55,7 @@ io.sockets.on('connection', function (socket) {
         // add the client's username to the global list
         usernames[username] = username;
         // echo to client they've connected
-        socket.emit('updatechat', 'SERVER', 'you have connected');
+        socket.emit('updatechat', 'SERVER', 'You have entered the chat room!');
         // echo globally (all clients) that a person has connected
         socket.broadcast.emit('updatechat', 'SERVER', username + ' has entered the chat room!');
         // update the list of users in chat, client-side
